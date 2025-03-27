13812c1114df8f1e5f8d41ad564f91e2b54b6384
Rajesh <rajesh4.ramesh@gmail.com>
2025-03-27 12:26:27 +0530

    project initialized

---
+++ src/main/java/com/microservices_with_sb/microservice_with_sb_department_service/MicroserviceWithSbDepartmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices_with_sb.microservice_with_sb_department_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceWithSbDepartmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceWithSbDepartmentServiceApplication.class, args);
+	}
+
+}
